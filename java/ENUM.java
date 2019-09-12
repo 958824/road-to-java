c332a03da27a190631a136af282ac7eb13c7a1b7
958824 <815423147@qq.com>
2019-09-12 17:03:21 +0800

    Update ENUM.java

--- java/ENUM.java
+++ java/ENUM.java
@@ -1,20 +1,19 @@
+package java;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+
 enum weekday{
     SUN,MON,TUS,WED,THU,FRI,SAT
-/*    MON(1),TUS(2),WED(3),THU(4),FRI(5),SAT(6),SUN(0);
-    private int value;
-    private weekday()
-    {
-        this.value=value;
-    }*/
 }
 class weeknode{
-
     private int id;
     private int type;
 
@@ -41,9 +40,7 @@ class weeknode{
     public String getName() {
         return name;
     }
-
     private String name;
-
 }
 public class ENUM {
     public static void main(String[] args) {
@@ -63,17 +60,36 @@ public class ENUM {
         }
         //测试stream流
         //过滤type=4的节点
-        List<weeknode> list1=list.stream().filter(node ->node.getType()!=4).collect(Collectors.toList()); //星期0 星期1 星期2 星期3 星期5 星期6
+        //List<weeknode> list1=list.stream().filter(node ->node.getType()!=4).collect(Collectors.toList());
+        List<weeknode> list1=list.stream().filter(node ->node.getType()!=weekday.THU.ordinal()).collect(toList());
         for(weeknode w:list1)
         {
-            System.out.print(w.getName()+" ");
+            System.out.print(w.getName()+" ");  //星期0 星期1 星期2 星期3 星期5 星期6
         }
         System.out.println();
 
-        Map<Integer,weeknode> list2=list.stream().collect(Collectors.toMap(weeknode::getId, Function.identity()));
+        //生成map
+        //Map<Integer,weeknode> list2=list.stream().collect(Collectors.toMap(weeknode::getId, Function.identity()));
+        Map<Integer,weeknode> list2=list.stream().collect(Collectors.toMap(weeknode::getId,str ->str));
         for(Map.Entry<Integer,weeknode> w:list2.entrySet())
         {
-            System.out.print(w.getValue().getName()+" ");
+            System.out.print(w.getValue().getName()+" ");  //星期0 星期1 星期2 星期3 星期4 星期5 星期6
         }
+        System.out.println();
+
+        //map
+        List<String> list3=list.stream().map(weeknode::getName).collect(toList());
+        for(String w:list3)
+        {
+            System.out.print(w+" ");  //星期0 星期1 星期2 星期3 星期5 星期6
+        }
+        System.out.println();
+
+        //peek
+        Map<Integer,weeknode> weeknodes=new HashMap<>();
+        System.out.println(list.stream().peek(weeknode->weeknodes.put(weeknode.getId(),weeknode)).count()); //7
+
+
+
     }
 }
